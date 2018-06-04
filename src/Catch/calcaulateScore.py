7d7506eb9ca28b5cd4c7b27f28ce5ef322ddee9b
chan <jiezehu@gmail.com>
2018-06-05 05:02:55 +0800

    calculate score

--- src/Catch/calcaulateScore.py
+++ src/Catch/calcaulateScore.py
@@ -22,41 +22,60 @@ def calculate():
         conn.rollback()
 
     for d in data:
-        print(d[0]+"   "+d[1]+"     "+d[3])
-        id=d[0]
-        school=d[3]
-        eval = d[4]
-        evalamount = d[5]
-        attendamount = d[6]
-        isCountry = d[7]
+        # print(d[0]+"   "+d[1]+"     "+d[3])
+        if(d[3]!=None):
+            id=d[0]
+            school=d[3]
+            eval = d[4]
+            evalamount = d[5]
+            attendamount = d[6]
+            isCountry = d[7]
 
-        while (school[0] != '-'):
+            while (school[0] != '-'):
+                school = school[1::]
             school = school[1::]
-        school = school[1::]
-        # get shoolscore
-        sql2 = "SELECT id FROM college WHERE name = '%s'" % school
-        try:
-            cursor.execute(sql2)
-            conn.commit()
-            data2=cursor.fetchone()
-            print("school id ++++++++++++++"+data2[0])
-        except:
-            conn.rollback()
-        if(data2 != None):
-            score=(600-data2[0])/ 600 * 30 + eval * evalamount / 5000 * 30 + attendamount / 10000 * 20 + isCountry * 20
+            # get shoolscore
+
+            sql2 = "SELECT id FROM college WHERE name = '%s'" % school
+            try:
+                cursor.execute(sql2)
+                conn.commit()
+                data2=cursor.fetchone()
+                print("school id ++++++++++++++"+data2[0])
+            except:
+                conn.rollback()
+
+
+            if(data2 != None):
+                score=(600-int(data2[0]))/ 600 * 30 + eval * evalamount / 5000 * 30 + attendamount / 10000 * 20 + isCountry * 20
+            else:
+                score=eval * evalamount / 5000 * 30 + attendamount / 10000 * 20 + isCountry * 20
+
+            print(score)
+
+            sql="UPDATE course SET CouScore = '%f' WHERE CouId='%s'"%(score,id)
+
+            try:
+                cursor.execute(sql)
+                conn.commit()
+                print("successfully update one!")
+            except:
+                conn.rollback()
         else:
-            score=eval * evalamount / 5000 * 30 + attendamount / 10000 * 20 + isCountry * 20
+            id = d[0]
+            eval = d[4]
+            evalamount = d[5]
+            attendamount = d[6]
+            score=eval*evalamount/2000*60+attendamount/1000*40;
+            print(score)
+            sql = "UPDATE course SET CouScore = '%f' WHERE CouId='%s'" % (score, id)
 
-        print(score)
-
-        sql="UPDATE course SET CouScore = '%f' WHERE CouId='%s'"%(score,id)
-
-        try:
-            cursor.execute(sql)
-            conn.commit()
-            print("successfully update one!")
-        except:
-            conn.rollback()
+            try:
+                cursor.execute(sql)
+                conn.commit()
+                print("successfully update one!")
+            except:
+                conn.rollback()
 
     conn.close()
 
